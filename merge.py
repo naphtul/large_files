70c3ef94cdaf5cf82f362af1a3175ce509879e24
Naphtali Gilead <naphtul@gmail.com>
2024-02-11 13:36:39 -0500

    Implementing the merge using mmap. So far, not improvement but rather degradation in performance.

--- merge.py
+++ merge.py
@@ -1,3 +1,5 @@
+import mmap
+
 from utils import timer, get_file_size
 
 
@@ -9,27 +11,29 @@ class MergeSortedFiles:
 
     @timer
     def merge_files(self):
-        with open(self.first, "r") as f1, open(self.second, "r") as f2, open(self.merged, "w") as f3:
-            f1.readline()
-            f2.readline()
+        with open(self.first, "r+b") as f1, open(self.second, "r+b") as f2, open(self.merged, "w") as f3:
+            mm1 = mmap.mmap(f1.fileno(), 0)
+            mm2 = mmap.mmap(f2.fileno(), 0)
+            mm1.readline()
+            mm2.readline()
+            f1_line = mm1.readline().decode("utf-8").strip()
+            f2_line = mm2.readline().decode("utf-8").strip()
             f3.write("ID,Sentence\n")
-            f1_line = f1.readline()
-            f2_line = f2.readline()
             while f1_line and f2_line:
                 f1_id, f1_sentence = f1_line.strip().split(",")
                 f2_id, f2_sentence = f2_line.strip().split(",")
                 if int(f1_id) < int(f2_id):
                     f3.write(f"{f1_id},{f1_sentence}\n")
-                    f1_line = f1.readline()
+                    f1_line = mm1.readline().decode("utf-8").strip()
                 else:
                     f3.write(f"{f2_id},{f2_sentence}\n")
-                    f2_line = f2.readline()
+                    f2_line = mm2.readline().decode("utf-8").strip()
             while f1_line:
-                f3.write(f1_line)
-                f1_line = f1.readline()
+                f3.write(f1_line + "\n")
+                f1_line = mm1.readline().decode("utf-8").strip()
             while f2_line:
-                f3.write(f2_line)
-                f2_line = f2.readline()
+                f3.write(f2_line + "\n")
+                f2_line = mm2.readline().decode("utf-8").strip()
         print(f"Sorted files ({get_file_size(self.first)}, {get_file_size(self.second)}) merged while sorting into {get_file_size(self.merged)}")
 
 
